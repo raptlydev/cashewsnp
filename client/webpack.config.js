3b1c038ab416545e0ffd4c8519d10bdc97f01282
mainak92 <31222142+mainak92@users.noreply.github.com>
2023-04-13 21:59:43 +0530

    webpack change

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -47,13 +47,13 @@ function publicPathURL(env) {
 function proxyTargetURL(env) {
     switch (env) {
         case 'development':
-            return 'https://cashewdatabase.herokuapp.com/';
+            return 'https://cashewsnp.herokuapp.com/';
         case 'qa':
-            return 'https://cashewdatabase.herokuapp.com/';
+            return 'https://cashewsnp.herokuapp.com/';
         case 'staging':
-            return 'https://cashewdatabase.herokuapp.com/';
+            return 'https://cashewsnp.herokuapp.com/';
         case 'production':
-            return 'https://cashewdatabase.herokuapp.com/';
+            return 'https://cashewsnp.herokuapp.com/';
     }
 }
 
@@ -83,7 +83,7 @@ function modeName(env) {
     }
 }
 
-const proxyTarget = !isProduction ? 'https://cashewdatabase.herokuapp.com/' : proxyTargetURL(buildMode);
+const proxyTarget = !isProduction ? 'https://cashewsnp.herokuapp.com/' : proxyTargetURL(buildMode);
 const envPath = !isProduction ? './.env-local' : `./.env-${modeFile(buildMode)}`;
 
 console.log(".env Used:", envPath);
