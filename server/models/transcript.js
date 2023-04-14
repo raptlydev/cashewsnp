bf74b993c311b67f01d8d57cfb23a849d21f4ff3
mainak92 <31222142+mainak92@users.noreply.github.com>
2023-04-14 15:50:07 +0530

    removed server

--- server/models/transcript.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const transcript = mongoose.Schema({
-    transcript: String
-}, { collection : 'transcripts' });
-
-const Transcript = mongoose.model('transcripts', transcript);
-
-export default Transcript;
