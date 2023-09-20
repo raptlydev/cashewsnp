890f1610f77fc989fb937e2f6a4490dd8ccb01d5
mainak92 <31222142+mainak92@users.noreply.github.com>
2023-09-20 19:36:05 +0530

    tutorial fix

--- client/src/components/tutorial/index.js
+++ client/src/components/tutorial/index.js
@@ -29,22 +29,127 @@ class Tutorial extends Component {
               <div className="container">
                 <div className="about-history-content">
 
-                  <div className="row">
+                  <div className="row section">
                     <div className="col-md-8 col-sm-12">
                       <div className="single-about-history">
                         <div className="about-history-txt">
-                          <p>CMDB database contain seven tabs in the sidebar of the each page.</p>
-                          <p><b>Home:</b> Contain brief introductory information about cashew and CMDB database.</p>
-                          <p><b>About:</b> Gives general information of microsatellite/ SSRs, methods of SSR markers development and classifications of SSRs.</p>
-                          <p><b>Database Search:</b> Allows retrieving or searching of CMDB for both genomic and genic microsatellite/ SSRs. Within the genomic and genic SSRs that are in silico mined and experimentally validated SSRs can be searched and downloaded using different search parameters.</p>
-                          <p><b>Analysis:</b> Provides statistical data of distribution of different SSRs in the genome and shoot transcriptomes of cashew.</p>
-                          <p><b>Tutorial:</b> Step by step guide on how to retrieve the microsatellite markers data from database based on different parameters.</p>
-                          <p><b>Resources:</b> Provides the links to the portal related to cashew crop and bioinformatics research.</p> 
-                          <p><b>Team/ Contact Us:</b> Details of the team members involved in the construction of CMDB database and Contact detail of the project investigator.</p>
+                          <p>The Cashew SNPs Database (CSNPDb) contains seven tabs in the sidebar of each page.</p>
+                          <p><b>Home:</b> Contain brief introductory information about cashew and the CSNPDb database.</p>
+                          <p><b>About:</b> Gives general information about single nucleotide polymorphisms (SNPs), methods of SNPs identification, and classifications of SNPs.</p>
+                          <p><b>Analysis:</b> Provides summary statistics of genome alignment, ACGT content of the resequenced genome, statistics for the identified SNP types, and a graphical representation of genome sequence data.</p>
+                          <p><b>Database Search:</b> Allows retrieval or searching of SNPs from the CSNPDb database using different parameters. The results are displayed after setting any criteria or parameters and clicking the search button.</p>
+                          <p><b>Tutorial:</b> A step-by step guide on how to retrieve the SNPs data from the database based on different parameters or criteria</p>
+                          <p><b>Resources:</b> Provides links to the portal related to cashew crop and bioinformatics research.</p> 
+                          <p><b>Team/ Contact Us:</b> Details of the team members involved in the construction of the CSNPDb database and contact details of the project investigator</p>
                         </div>
                       </div>
                     </div>
 
+                  </div>
+
+                  <div className='row section'>
+                    {/* Image Section  */}
+                    <div className="col-md-10 col-sm-12">
+								      <div className="about-history-txt">
+									      <div className="single-about-history">
+									        <div className="about-history-img">
+										        <img src="assets/images/tutorial/Pic1.png" alt="about" /> 
+									        </div>
+									      </div>
+								      </div>
+							      </div>
+                    {/* End of Image Section */}
+                    <div className="col-md-10 col-sm-12">
+                      <div className="single-about-history">
+                        <div className="about-history-txt">
+                          <p><b>#Home</b> page contains the basic description about the Cashew, and CSNPDb database.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className='row section'>
+                    {/* Image Section  */}
+                    <div className="col-md-10 col-sm-12">
+								      <div className="about-history-txt">
+									      <div className="single-about-history">
+									        <div className="about-history-img">
+										        <img src="assets/images/tutorial/Pic2.png" alt="about" /> 
+									        </div>
+									      </div>
+								      </div>
+							      </div>
+                    {/* End of Image Section */}
+                    <div className="col-md-10 col-sm-12">
+                      <div className="single-about-history">
+                        <div className="about-history-txt">
+                          <p><b>#About</b> page provides general information of Single nucleotide polymorphisms (SNPs).</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className='row section'>
+                    {/* Image Section  */}
+                    <div className="col-md-10 col-sm-12">
+								      <div className="about-history-txt">
+									      <div className="single-about-history">
+									        <div className="about-history-img">
+										        <img src="assets/images/tutorial/Pic3.png" alt="about" /> 
+									        </div>
+									      </div>
+								      </div>
+							      </div>
+                    {/* End of Image Section */}
+                    <div className="col-md-10 col-sm-12">
+                      <div className="single-about-history">
+                        <div className="about-history-txt">
+                          <p><b>#Analysis page:</b> Provides Summary statistics of Genome Alignment, ACGT content of Resequenced Genome, Statistics for the identified SNP types and graphical representation of genome sequence data.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className='row section'>
+                    {/* Image Section  */}
+                    <div className="col-md-10 col-sm-12">
+								      <div className="about-history-txt">
+									      <div className="single-about-history">
+									        <div className="about-history-img">
+										        <img src="assets/images/tutorial/Pic4.png" alt="about" /> 
+									        </div>
+									      </div>
+								      </div>
+							      </div>
+                    {/* End of Image Section */}
+                    <div className="col-md-10 col-sm-12">
+                      <div className="single-about-history">
+                        <div className="about-history-txt">
+                          <p><b>#Database Search:</b> Allows retrieving or searching of CSNPDb for SNPs.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className='row section'>
+                    {/* Image Section  */}
+                    <div className="col-md-10 col-sm-12">
+								      <div className="about-history-txt">
+									      <div className="single-about-history">
+									        <div className="about-history-img">
+										        <img src="assets/images/tutorial/Pic5.png" alt="about" /> 
+									        </div>
+									      </div>
+								      </div>
+							      </div>
+                    {/* End of Image Section */}
+                    <div className="col-md-10 col-sm-12">
+                      <div className="single-about-history">
+                        <div className="about-history-txt">
+                          <p><b>#Resources</b> page provides the links to the portal related to cashew crop and bioinformatics research and databases.</p>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
